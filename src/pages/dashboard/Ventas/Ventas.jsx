ccfc8eb853fcbc2163b1498f53d4d1996de46748
Jonathan Torres <racknarow1@gmail.com>
2025-06-17 21:35:01 -0500

    add cambio alert

--- src/pages/dashboard/Ventas/Ventas.jsx
+++ src/pages/dashboard/Ventas/Ventas.jsx
@@ -10,200 +10,224 @@ import Modal from '../../../components/modal/Modal';
 import ModalPerecedero from '../../../components/ventas/modalPerecedero/ModalPerecedero';
 
 const Ventas = () => {
-  const httpService = new HttpService();
-  const selectedStore = useStore((s) => s.selectedStore);
+    const httpService = new HttpService();
+    const selectedStore = useStore((s) => s.selectedStore);
 
-  const totalCompra = useStore((state) => state.totalCompra);
-  const setTotalCompra = useStore((state) => state.setTotalCompra);
+    const totalCompra = useStore((state) => state.totalCompra);
+    const setTotalCompra = useStore((state) => state.setTotalCompra);
 
-  const totalCompraSinCupon = useStore((state) => state.totalCompraSinCupon);
-  const setTotalCompraSinCupon = useStore((state) => state.setTotalCompraSinCupon);
-
-  const selectedCoupon = useStore((state) => state.selectedCoupon);
-
-  const cartItems = useStore((state) => state.cartItems);
-  const setCartItems = useStore((state) => state.setCartItems);
-
-
-  const [productData, setProductData] = useState([]);
-  const [loadingProducts, setLoadingProducts] = useState(false);
-  const [searchTerm, setSearchTerm] = useState('');
-
-  // const [cartItems, setCartItems]   = useState([]);
-
-  const [showModal, setShowModal] = useState(false);
-  const [selectedProduct, setSelectedProduct] = useState(null);
-
- // Para darle estilo al ultimo producto agregado al carrito
-  const [lastAddedIndex, setLastAddedIndex] = useState(null);
-
-  useEffect(() => {
-     console.log('cartItems useEffect', cartItems);
-
-    // 1) subtotal sin descuentos
-    const base = cartItems.reduce(
-      (acc, item) => acc + (parseFloat(item.product.salePrice) || 0),
-      0
+    const totalCompraSinCupon = useStore((state) => state.totalCompraSinCupon);
+    const setTotalCompraSinCupon = useStore(
+        (state) => state.setTotalCompraSinCupon
     );
-    // setTotalCompraSinCupon(base);
-    // setTotalCompra(base);
-  }, [cartItems]);
 
+    const selectedCoupon = useStore((state) => state.selectedCoupon);
 
-  // — efecto: recarga al cambiar tienda —
-  useEffect(() => {
-    fetchProducts();
-    
-  }, [selectedStore]);
+    const cartItems = useStore((state) => state.cartItems);
+    const setCartItems = useStore((state) => state.setCartItems);
 
-  const fetchProducts = async () => {
-    console.log('ACTUALIZANDO PRODUCTOS');
+    const dataCambio = useStore((state) => state.dataCambio);
 
-    setCartItems([]); 
-    setLoadingProducts(true);
-    try {
-      const res = await httpService.getData(`/product?storeId=${selectedStore}`);
-      if (res.status === 200) setProductData(res.data || []);
-      else errorAlert('Error', 'No se pudo obtener la lista de productos');
-    } catch {
-      errorAlert('Error', 'No se pudo obtener la lista de productos');
-    } finally {
-      setLoadingProducts(false);
-    }
-  };
+    const [productData, setProductData] = useState([]);
+    const [loadingProducts, setLoadingProducts] = useState(false);
+    const [searchTerm, setSearchTerm] = useState('');
 
-  const filteredProducts = productData.filter(p => {
-    const term = searchTerm.toLowerCase();
-    return p.name.toLowerCase().includes(term) || p.code.toLowerCase().includes(term);
-  });
+    // const [cartItems, setCartItems]   = useState([]);
 
-  // — este es el “handler” que añade el primer stockunit disponible —
-  const handleSelectProduct = (product) => {
-    if (product.perishable) {
-      return handleSelectPerecedero(product);
-    }
+    const [showModal, setShowModal] = useState(false);
+    const [selectedProduct, setSelectedProduct] = useState(null);
 
+    // Para darle estilo al ultimo producto agregado al carrito
+    const [lastAddedIndex, setLastAddedIndex] = useState(null);
 
-    // 1) si no hay unidades, nada que hacer
-    if (!product.stockunit?.length) {
-      return errorAlert('Sin stock', 'No hay unidades disponibles de este producto');
-    }
-    // 2) tomamos la primera unidad (o la más antigua, etc.)
-    const unit = product.stockunit[0];
+    useEffect(() => {
+        console.log('cartItems useEffect', cartItems);
 
-    const cartUnit = {
-      ...unit,                               // id, product, expirationDate, etc.
-      itemCoupon: null,                      // cupón propio (aún sin asignar)
-      priceWithItemCoupon: Number(unit.product.salePrice), // precio “neto” inicial
+        // 1) subtotal sin descuentos
+        const base = cartItems.reduce(
+            (acc, item) => acc + (parseFloat(item.product.salePrice) || 0),
+            0
+        );
+        // setTotalCompraSinCupon(base);
+        // setTotalCompra(base);
+    }, [cartItems]);
+
+    // — efecto: recarga al cambiar tienda —
+    useEffect(() => {
+        fetchProducts();
+    }, [selectedStore]);
+
+    const fetchProducts = async () => {
+        console.log('ACTUALIZANDO PRODUCTOS');
+
+        setCartItems([]);
+        setLoadingProducts(true);
+        try {
+            const res = await httpService.getData(
+                `/product?storeId=${selectedStore}`
+            );
+            if (res.status === 200) setProductData(res.data || []);
+            else
+                errorAlert('Error', 'No se pudo obtener la lista de productos');
+        } catch {
+            errorAlert('Error', 'No se pudo obtener la lista de productos');
+        } finally {
+            setLoadingProducts(false);
+        }
     };
 
-    // 3) Removemos la unidad del array de unidades
-    const updatedStockUnits = product.stockunit.filter((_, index) => index !== 0);
-
-    // 4) Actualizamos el producto con las unidades restantes
-    const updatedProduct = { ...product, stockunit: updatedStockUnits };
-    
-    // 5) Actualizar el productData con el producto actualizado
-    const updatedProductData = productData.map(p => p.id === product.id ? updatedProduct : p);
-    setProductData(updatedProductData);
-
-    // // 6) Añadir al carrito
-    // setCartItems(prev => [...prev, unit]);
-
-    // 6) Añadir al carrito y marcar el índice recién añadido
-    // setCartItems(prev => {
-    //   const newIndex = prev.length;
-    //     setLastAddedIndex(newIndex);
-    //    // después de 1s, quita el highlight
-    //     setTimeout(() => setLastAddedIndex(null), 100);
-    //     return [...prev, unit];
-    //  });
-
-    const newIndex = cartItems.length;          
-    setLastAddedIndex(newIndex);
-    setTimeout(() => setLastAddedIndex(null), 100);
-    // setCartItems([...cartItems, unit]);
-    setCartItems([...cartItems, cartUnit]);  // ⬅️ahora push el OBJETO nuevo 
-  
-  };
-
-  const handleSelectPerecedero = (product) => {
-
-    if (!product.stockunit?.length) {
-      return errorAlert('Sin stock', 'No hay unidades disponibles de este producto');
-    }
-
-    //Abrir modal para seleccionar el producto perecedero
-    setShowModal(true)
-    setSelectedProduct(product); // Guardar el producto perecedero seleccionado
-  }
-
-
-  // — quita del carrito según stockUnitId único —
-  const handleRemoveFromCart = (unit) => {
-    // Remover el stock unit del carrito
-    setCartItems(cartItems.filter(item => item.id !== unit.id));
-
-    // Añadir el stock unit de vuelta al producto perecedero
-    const updatedProductData = productData.map(p => {
-      if (p.id === unit.product.id) {
-        return {
-          ...p,
-          stockunit: [...p.stockunit, unit], // Añadir la unidad de vuelta al stock
-        };
-      }
-      return p;
+    const filteredProducts = productData.filter((p) => {
+        const term = searchTerm.toLowerCase();
+        return (
+            p.name.toLowerCase().includes(term) ||
+            p.code.toLowerCase().includes(term)
+        );
     });
-    setProductData(updatedProductData);
-  };
 
-  return (
-    <div className="container-fluid mt-4 main_container">
-      <h1 className="text-center mb-5">Panel de Ventas</h1>
-      <div className="row bg-light">
-        <SaleSearch
-          searchTerm={searchTerm}
-          setSearchTerm={setSearchTerm}
-          loadingProducts={loadingProducts}
-          filteredProducts={filteredProducts}
-          handleSelectProduct={handleSelectProduct}
-        />
+    // — este es el “handler” que añade el primer stockunit disponible —
+    const handleSelectProduct = (product) => {
+        if (product.perishable) {
+            return handleSelectPerecedero(product);
+        }
 
-        <SalePanel
-          cartItems={cartItems}
-          onRemoveFromCart={handleRemoveFromCart}
-          lastAddedIndex={lastAddedIndex}
-          fetchProducts={fetchProducts}
-        />
-      </div>
+        // 1) si no hay unidades, nada que hacer
+        if (!product.stockunit?.length) {
+            return errorAlert(
+                'Sin stock',
+                'No hay unidades disponibles de este producto'
+            );
+        }
+        // 2) tomamos la primera unidad (o la más antigua, etc.)
+        const unit = product.stockunit[0];
 
-      {/* Modal para agregar productos perecederos */}
-      <Modal show={showModal} onClose={() => setShowModal(false)} title="Agregar producto perecedero">
-        <ModalPerecedero
-          selectedProduct={selectedProduct}
-          setSelectedProduct={setSelectedProduct}
-          setProductData={setProductData}
-          setShowModal={setShowModal}
-          productData={productData}
-          setLoadingProducts={setLoadingProducts}
-          setLastAddedIndex={setLastAddedIndex}
+        const cartUnit = {
+            ...unit, // id, product, expirationDate, etc.
+            itemCoupon: null, // cupón propio (aún sin asignar)
+            priceWithItemCoupon: Number(unit.product.salePrice), // precio “neto” inicial
+        };
 
-        />
-        <div className="modal-footer">
-          <button
-            className="btn btn-danger"
-            onClick={() => {
-              // Clear the modal and reset selected product
-              setShowModal(false);
-              setSelectedProduct(null);
-            }}
-          >
-            Cerrar
-          </button>
+        // 3) Removemos la unidad del array de unidades
+        const updatedStockUnits = product.stockunit.filter(
+            (_, index) => index !== 0
+        );
+
+        // 4) Actualizamos el producto con las unidades restantes
+        const updatedProduct = { ...product, stockunit: updatedStockUnits };
+
+        // 5) Actualizar el productData con el producto actualizado
+        const updatedProductData = productData.map((p) =>
+            p.id === product.id ? updatedProduct : p
+        );
+        setProductData(updatedProductData);
+
+        // // 6) Añadir al carrito
+        // setCartItems(prev => [...prev, unit]);
+
+        // 6) Añadir al carrito y marcar el índice recién añadido
+        // setCartItems(prev => {
+        //   const newIndex = prev.length;
+        //     setLastAddedIndex(newIndex);
+        //    // después de 1s, quita el highlight
+        //     setTimeout(() => setLastAddedIndex(null), 100);
+        //     return [...prev, unit];
+        //  });
+
+        const newIndex = cartItems.length;
+        setLastAddedIndex(newIndex);
+        setTimeout(() => setLastAddedIndex(null), 100);
+        // setCartItems([...cartItems, unit]);
+        setCartItems([...cartItems, cartUnit]); // ⬅️ahora push el OBJETO nuevo
+    };
+
+    const handleSelectPerecedero = (product) => {
+        if (!product.stockunit?.length) {
+            return errorAlert(
+                'Sin stock',
+                'No hay unidades disponibles de este producto'
+            );
+        }
+
+        //Abrir modal para seleccionar el producto perecedero
+        setShowModal(true);
+        setSelectedProduct(product); // Guardar el producto perecedero seleccionado
+    };
+
+    // — quita del carrito según stockUnitId único —
+    const handleRemoveFromCart = (unit) => {
+        // Remover el stock unit del carrito
+        setCartItems(cartItems.filter((item) => item.id !== unit.id));
+
+        // Añadir el stock unit de vuelta al producto perecedero
+        const updatedProductData = productData.map((p) => {
+            if (p.id === unit.product.id) {
+                return {
+                    ...p,
+                    stockunit: [...p.stockunit, unit], // Añadir la unidad de vuelta al stock
+                };
+            }
+            return p;
+        });
+        setProductData(updatedProductData);
+    };
+
+    return (
+        <div className="container-fluid mt-4 main_container">
+            <h1 className="text-center mb-5">
+              {
+                dataCambio?.cambioActivo === true ? "Panel de Cambio" : "Panel de Ventas"
+              }
+            </h1>
+            <div className="row bg-light">
+                <SaleSearch
+                    searchTerm={searchTerm}
+                    setSearchTerm={setSearchTerm}
+                    loadingProducts={loadingProducts}
+                    filteredProducts={filteredProducts}
+                    handleSelectProduct={handleSelectProduct}
+                />
+
+                {dataCambio?.cambioActivo === true ? (
+                    <div className="col-md-4 fs-3 fw-bold">Panel de cambio En construción...</div>
+                ) : (
+                    <SalePanel
+                        cartItems={cartItems}
+                        onRemoveFromCart={handleRemoveFromCart}
+                        lastAddedIndex={lastAddedIndex}
+                        fetchProducts={fetchProducts}
+                    />
+                )}
+            </div>
+
+            {/* Modal para agregar productos perecederos */}
+            <Modal
+                show={showModal}
+                onClose={() => setShowModal(false)}
+                title="Agregar producto perecedero"
+            >
+                <ModalPerecedero
+                    selectedProduct={selectedProduct}
+                    setSelectedProduct={setSelectedProduct}
+                    setProductData={setProductData}
+                    setShowModal={setShowModal}
+                    productData={productData}
+                    setLoadingProducts={setLoadingProducts}
+                    setLastAddedIndex={setLastAddedIndex}
+                />
+                <div className="modal-footer">
+                    <button
+                        className="btn btn-danger"
+                        onClick={() => {
+                            // Clear the modal and reset selected product
+                            setShowModal(false);
+                            setSelectedProduct(null);
+                        }}
+                    >
+                        Cerrar
+                    </button>
+                </div>
+            </Modal>
         </div>
-      </Modal>
-    </div>
-  );
+    );
 };
 
 export default Ventas;
